58268ba93fd963d470ef4c2c10196f7fbef01281
Tommie Terhoeve <tommieterhoeve@gmail.com>
2018-05-05 18:33:18 +0200

    Assignment 1 finished

---
+++ input_output.cpp
@@ -0,0 +1,126 @@
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <wiringPi.h>
+#include <fstream>
+
+#define OUTPUT_PIN 0
+#define INPUT_PIN 2
+
+using namespace std;
+
+char decodeMorse(string s) {
+  const string morse("EISHVUF*ARL*WPJTNDBXKCYMGZQO");
+  int p = -1;
+  for (int i = 0; i < s.length(); i++) {
+    if (s[i] == '-') p += 1 << (4 - i);
+    else p++;
+  }
+  return morse[p];
+}
+
+//not relevant for assignment
+void switchChanged() {
+  static string buffer;
+  static unsigned int previousTime = 0, currentTime, timePassed;
+  currentTime = millis();
+  if (previousTime == 0) { // first pulse
+    previousTime = currentTime;
+    return;
+  }
+  timePassed = currentTime - previousTime;
+  previousTime = currentTime;
+  if (digitalRead(INPUT_PIN) == LOW) { // switch pressed
+    if (timePassed > 200) {
+
+      // TO DO
+
+    }
+    if (timePassed > 600) {
+
+      // TO DO
+
+    }
+  } else { // switch released
+    if (timePassed > 200) {
+
+      // TO DO
+
+    }
+    else {
+
+      // TO DO
+
+    }
+  }
+}
+
+void wait(int n) {
+  delay(100 * n);
+}
+
+void dot() {
+  digitalWrite(OUTPUT_PIN, HIGH);
+  wait(1);
+  digitalWrite(OUTPUT_PIN, LOW);
+  wait(1);
+
+}
+
+void dash() {
+  digitalWrite(OUTPUT_PIN, HIGH);
+  wait(3);
+  digitalWrite(OUTPUT_PIN, LOW);
+  wait(1);
+
+}
+//Open a text file which contains the morse codes
+string sendMorse(char character) {
+  string characterInMorse;
+  string line;
+  freopen("morse.txt", "r", stdin);
+
+  while(getline(cin, line)){   //Searches for line which contains the given character
+       if(line.at(0) == character){
+         cout << line;
+         break;
+       }
+  }
+
+  for(int index = 1; index < line.length() - 1; index = index + 1){ //Checks every symbol on the selected line for dashes and dots.
+    if(line.at(index) == '.'){
+      dot();
+      characterInMorse += '.';
+    }
+    else if(line.at(index == '-')){
+      dash();
+      characterInMorse += '-';
+    }
+  }
+  wait(2); // wait 1 + 2 = 3 dots in total
+  characterInMorse += " "; //space between characters for translation to ASCII
+  return characterInMorse;
+}
+
+int main() {
+
+  wiringPiSetup() ;
+  pullUpDnControl(INPUT_PIN, PUD_UP);
+  pinMode (OUTPUT_PIN, OUTPUT);
+  wiringPiISR(INPUT_PIN, INT_EDGE_BOTH, &switchChanged);
+
+  cout << "ASCII to morse code" << endl;
+  cout << "Please enter a sentence" << endl;
+  string input;
+  string output;
+  getline(cin, input);
+
+  for(int i = 0; i < input.length(); i = i + 1){
+    if(input.at(i) == ' '){
+      output += " ";
+      wait(7);
+    }
+    else output.append(sendMorse(input.at(i)));
+  }
+  cout << "Output " + output;
+}
